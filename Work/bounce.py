43f85a0f5da4defebee908a50085827a21addd52
rabnic <mrnicrab@gmail.com>
2020-06-01 00:38:03 +0200

    Completed Exercise 1.5: The Bouncing Ball

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,7 @@
-# bounce.py
-#
-# Exercise 1.5
+height = 100  # meters
+after_bounce_height = 0.6  # Each time ball hits the ground, it bounces back up to 3/5 the height it fell
+first_ten_bounces = 10
+
+for bounce in range(1, first_ten_bounces+1):
+    print(bounce, round(height * after_bounce_height, 4))
+    height *= after_bounce_height
