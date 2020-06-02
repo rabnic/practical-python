04654d08f4d54408f150d8f282b826cfaa9c400d
rabnic <mrnicrab@gmail.com>
2020-06-02 21:43:10 +0200

    Renamed variable months to month

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -5,16 +5,16 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
-months = 0
+month = 0
 
 # If wish to speed up payments by paying extra
 extra_payment_start_month = 60
 extra_payment_end_month = 108
-extra_payment = 1000
+extra_payment = 1000.0
 
 while principal > 0:
-    # Pay extra if months is in extra payments period
-    if extra_payment_start_month <= months <= extra_payment_end_month:
+    # Pay extra if month is in extra payments period
+    if extra_payment_start_month <= month <= extra_payment_end_month:
         principal = principal * (1 + rate / 12) - (payment + extra_payment)
         if not principal > 0:
             break
@@ -24,8 +24,8 @@ while principal > 0:
         if not principal > 0:
             break
         total_paid += payment
-    months += 1
-    print(f"{months} {round(total_paid, 2)} {round(principal, 2)}")
+    month += 1
+    print(f"{month} {round(total_paid, 2)} {round(principal, 2)}")
 
 print(f"Total paid: {round(total_paid, 2)}")
-print(f"Months {months}")
+print(f"Months {month}")
