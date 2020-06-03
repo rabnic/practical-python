0c86ea4eea157318ce2b096331b5904394fca8bf
rabnic <mrnicrab@gmail.com>
2020-06-03 23:33:10 +0200

    Completed Exercise 2.6: Dictionaries as a container

--- Work/report.py
+++ Work/report.py
@@ -14,3 +14,16 @@ def read_portfolio(portfolio_file):
             dict_holder = {"name": row[0], "shares": int(row[1]), "price": float(row[2])}
             portfolio_list.append(dict_holder)
     return portfolio_list
+
+
+def read_prices(prices_file):
+    """
+
+    :param prices_file:
+    :return: dict of stock prices where key=stock name and value=price
+    """
+    with open(prices_file) as file:
+        rows = csv.reader(file)
+        price_dict = {stock[0]: float(stock[1]) for stock in rows if stock}
+    return price_dict
+
