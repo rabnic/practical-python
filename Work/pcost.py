e1d4d3186229f61a60376af627d9e68db6213698
rabnic <mrnicrab@gmail.com>
2020-06-03 15:53:10 +0200

    Completed Exercise 1.30: Refactored script to use functions for organisation

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,17 +1,26 @@
-# pcost.py
-#
-# Exercise 1.27
+# Display total formatted
+def display_total(total):
+    print(f'Total cost: R{total:.2f}')
 
-# Initialise total to R0.0
-total_amount = 0.0
-with open('Data/portfolio.csv', 'rt') as file:
-    # Discard the titles
-    headers = next(file)
-    # Iterate over the stocks and sum all
-    for line in file:
-        stock_line = line.split(sep=',')
-        shares = int(stock_line[1])
-        price = float(stock_line[2])
-        total_amount += shares * price
 
-print(f'Total cost: R{total_amount:.2f}')
+# Calculate the total cost of all stocks
+def portfolio_cost(csv_file):
+    """
+    :param csv_file:
+    :return: total cost of stocks on provided portfolio
+    """
+    # Initialise total to R0.0
+    total_amount = 0.0
+    with open(csv_file, 'rt') as file:
+        # Discard the titles
+        headers = next(file)
+        # Iterate over the stocks and sum all
+        for line in file:
+            stock_line = line.split(sep=',')
+            shares = int(stock_line[1])
+            price = float(stock_line[2])
+            total_amount += shares * price
+    return total_amount
+
+
+display_total(portfolio_cost('Data/portfolio.csv'))
