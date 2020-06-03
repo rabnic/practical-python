b1990393af7029a7fd8349eb4a26dbde13a5639c
rabnic <mrnicrab@gmail.com>
2020-06-03 15:35:51 +0200

    Completed Exercise 1.27: Calculate total cost of all stocks

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
+
+# Initialise total to R0.0
+total_amount = 0.0
+with open('Data/portfolio.csv', 'rt') as file:
+    # Discard the titles
+    headers = next(file)
+    # Iterate over the stocks and sum all
+    for line in file:
+        stock_line = line.split(sep=',')
+        shares = int(stock_line[1])
+        price = float(stock_line[2])
+        total_amount += shares * price
+
+print(f'Total cost: R{total_amount:.2f}')
