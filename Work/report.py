ec6e673674ab4d8242b4b521925bc284333fadf1
rabnic <mrnicrab@gmail.com>
2020-06-03 23:51:41 +0200

    Completed Exercise 2.7: Finding out if you can retire

--- Work/report.py
+++ Work/report.py
@@ -27,3 +27,18 @@ def read_prices(prices_file):
         price_dict = {stock[0]: float(stock[1]) for stock in rows if stock}
     return price_dict
 
+
+portfolio = read_portfolio("Data/portfolio.csv")
+prices = read_prices("Data/prices.csv")
+
+# Calculate the total cost of portfolio
+total_cost = sum([stock['shares'] * stock['price'] for stock in portfolio])
+print(f'Total cost: R{total_cost}')
+
+# Get the current value of portfolio
+total_value = 0.0
+for stock in portfolio:
+    total_value += stock['shares'] * prices[stock['name']]
+
+print(f'Current value: R{total_value}')
+print(f'Gain :R{total_value - total_cost:.2f}')
