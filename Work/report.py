596e851bbcb75d33ad7786adf6e96fc63d63a73d
rabnic <mrnicrab@gmail.com>
2020-06-03 22:45:40 +0200

    Completed Exercise 2.5: List of Dictionaries

--- Work/report.py
+++ Work/report.py
@@ -4,12 +4,13 @@ import csv
 def read_portfolio(portfolio_file):
     """
     :param portfolio_file:
-    :return: list of tuples for stock portfolio read from file given
+    :return: list of dicts for stock portfolio read from file given
     """
     portfolio_list = []
     with open(portfolio_file) as file:
         rows = csv.reader(file)
         headers = next(rows)
         for row in rows:
-            portfolio_list.append((row[0], int(row[1]), float(row[2])))
+            dict_holder = {"name": row[0], "shares": int(row[1]), "price": float(row[2])}
+            portfolio_list.append(dict_holder)
     return portfolio_list
