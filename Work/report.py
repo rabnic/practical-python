2dd95332266bd4204551b3c3ca295bd6b82e0ab5
rabnic <mrnicrab@gmail.com>
2020-06-03 22:27:01 +0200

    Completed Exercise 2.4: A list of tuples

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,15 @@
-# report.py
-#
-# Exercise 2.4
+import csv
+
+
+def read_portfolio(portfolio_file):
+    """
+    :param portfolio_file:
+    :return: list of tuples for stock portfolio read from file given
+    """
+    portfolio_list = []
+    with open(portfolio_file) as file:
+        rows = csv.reader(file)
+        headers = next(rows)
+        for row in rows:
+            portfolio_list.append((row[0], int(row[1]), float(row[2])))
+    return portfolio_list
