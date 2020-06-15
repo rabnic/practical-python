239703818fe538f45d5afdcaea726199a0c06ff0
rabnic <mrnicrab@gmail.com>
2020-06-16 00:41:38 +0200

    Modified for Exercise 3.7 to use csv.DictReader

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,22 +1,26 @@
 import csv
 
 
-def parse_csv(filename):
+def parse_csv(filename, select=None):
     """
     Parse a csv file into a list of dict records
     :param filename:
     :return: List of dictionaries
     """
     with open(filename) as file:
-        rows = csv.reader(file)
+        rows = csv.DictReader(file)
 
-        # Read the file headers
-        headers = next(rows)
         records = []
         for row in rows:
             if not row:  # Skip rows with no data
                 continue
-            record = dict(zip(headers, row))
+            row = dict(row)
+            if select:  # If user wants selected columns
+                row = {k: row[k] for k in row.keys() if k in select}
+            record = row
             records.append(record)
 
         return records
+
+
+print(parse_csv('Data/portfolio.csv', select=['name', 'shares']))
